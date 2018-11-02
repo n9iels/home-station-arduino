4ae147cfed95d0854776801d6786acc97b328f98
Niels van der Veer <n.i.vdveer@gmail.com>
2018-11-02 15:53:50 +0100

    Improve calculation of the windspeed module

--- wind/wind.ino
+++ wind/wind.ino
@@ -1,3 +1,4 @@
+#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
@@ -10,14 +11,35 @@ const char *password = "";
 
 // Global declarations
 unsigned long startTime;
+unsigned long readings[50];
 int currentHallState = LOW;
 int prevHallState = HIGH;
 int rotations = 0;
-int rpm = 0;
 
 void setup()
 {
     Serial.begin(115200);
+
+    //Start, configurare and connect to WiFi
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    // while (WiFi.status() != WL_CONNECTED)
+    // {
+    //     delay(500);
+    //     Serial.print(".");
+    // }
+
+    // Verbonden.
+    Serial.println("Connected!");
+
+    // Access Point (SSID).
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // IP adres.
+    Serial.print("IP: ");
+    Serial.println(WiFi.localIP());
 
     // Setup pins
     pinMode(HALLPIN, INPUT);
@@ -28,27 +50,32 @@ void setup()
 
 void loop()
 {
-    messuareRpm();
-}
-
-void messuareRpm()
-{
     currentHallState = digitalRead(HALLPIN);
 
     if (currentHallState == LOW && currentHallState != prevHallState)
     {
         rotations += 1;
+
+        if (rotations % 2 == 0)
+        {
+            // Array do start at 0
+            int index = (rotations / 2) - 1;
+            int difference = millis() - startTime;
+
+            // Use a offset of minus 2, since one complete rotation has two magnets
+            readings[index] = difference;
+        }
     }
 
     prevHallState = currentHallState;
 
-    // Caculate the RPM every minute (60000 ms)
+    // Caculate the RPM every minute (300000ms = 5 minuten)
     if (millis() >= startTime + 10000)
     {
-        rpm = calculateRpm();
-        Serial.println(rpm);
+        sendWindData();
         startTime = millis();
         rotations = 0;
+        memset(readings, 0, sizeof(readings));
     }
 }
 
@@ -57,37 +84,37 @@ int calculateRpm()
     return (int)rotations / 2;
 }
 
-// bool sendAtmosphereData()
-// {
-//     HTTPClient http;
-//     String payload;
-//     DynamicJsonBuffer jsonBuffer(JSON_OBJECT_SIZE(3));
+bool sendWindData()
+{
+    HTTPClient http;
+    String payload;
+    DynamicJsonBuffer jsonBuffer(JSON_OBJECT_SIZE(3));
 
-//     // Build JSON payload
-//     JsonObject &root = jsonBuffer.createObject();
-//     root["temperature"] = temperature;
-//     root["humidity"] = humidity;
-//     root["heatIndex"] = heatIndex;
+    // Build JSON payload
+    JsonObject &root = jsonBuffer.createObject();
+    JsonArray &readingsArray = jsonBuffer.createArray();
+    readingsArray.copyFrom(readings);
 
-//     root.printTo(payload);
+    root["readings"] = readingsArray;
 
-//     // Build HTTP POST
-//     http.begin("http://192.168.1.148:8080/api/weather/atmosphere");
-//     http.addHeader("Content-Type", "application/json");
-//     http.addHeader("User-Agent", "Wemos D1 Mini - Atmosphere");
+    root.printTo(payload);
 
-//     int httpCode = http.POST(payload);
+    // Build HTTP POST
+    http.begin("http://weer.nielsvanderveer.nl:1686/api/weather/wind");
+    http.addHeader("Content-Type", "application/json");
+    http.addHeader("User-Agent", "Wemos D1 Mini - Windmeter");
 
-//     if (httpCode != 201)
-//     {
-//         drawCenterText("Faild to send data.");
-//         Serial.println("Faild to send data.");
-//         Serial.println(httpCode);
-//         Serial.println(payload);
-//         return false;
-//     }
+    int httpCode = http.POST(payload);
 
-//     http.end();
+    if (httpCode != 201)
+    {
+        Serial.println("Faild to send data.");
+        Serial.println(httpCode);
+        Serial.println(payload);
+        return false;
+    }
 
-//     return true;
-// }
+    http.end();
+
+    return true;
+}
